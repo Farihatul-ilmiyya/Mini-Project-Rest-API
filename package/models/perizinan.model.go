ddef0508e962f85bd32b25997d7dbc18e838d977
Mia <farihatulilmiyya05@gmail.com>
2022-05-18 21:21:10 +0700

    repair code

--- package/models/perizinan.model.go
+++ package/models/perizinan.model.go
@@ -9,11 +9,11 @@ type Perizinan struct {
 	Id_Perizinan        	int       	`gorm:"primaryKey" json:"id_perizinan" form:"id_perizinan"`
 	Id_Karyawan        		string    	`gorm:"foreignKey" json:"id_karyawan" form:"id_karyawan"`
 	Nama					string		`json:"nama" form:"nama"`
-	Tgl_Mulai				time.Time		`json:"tgl_mulai" form:"tgl_mulai"`
-	Tgl_Selesai				time.Time		`json:"tgl_selesai" form:"tgl_selesai"`
+	Tgl_Mulai				time.Time	`json:"tgl_mulai" form:"tgl_mulai"`
+	Tgl_Selesai				time.Time	`json:"tgl_selesai" form:"tgl_selesai"`
 	Keterangan_Perizinan	string		`json:"keterangan_perizinan" form:"keterangan_perizinan"`
-	Created_At         		time.Time 		`json:"created_at" form:"created_at"`
-	Updated_At         		time.Time 		`json:"updated_at" form:"updated_at"`
+	Created_At         		time.Time 	`json:"created_at" form:"created_at"`
+	Updated_At         		time.Time 	`json:"updated_at" form:"updated_at"`
 }
 
 func (Perizinan) TableName() string {
